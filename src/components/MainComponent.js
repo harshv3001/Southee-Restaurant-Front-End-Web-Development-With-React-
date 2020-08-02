af9607dd77c4220b204d2d37874fae2629b4bdd1
Harsh Verma <harsh1999verma.hv@gmail.com>
2020-08-03 01:00:46 +0530

    Header and Footer

---
+++ src/components/MainComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent'; 
+import Dishdetail from './DishdetailComponent'
+import Header from './HeaderComponent';
+import Footer from './FooterComponent'
+import {DISHES} from '../shared/dishes';
+
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+      SelectedDish: null
+    };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({ SeletedDish: dishId});
+}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Menu dishes={this.state.dishes}
+            onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Dishdetail dish = {this.state.dishes.filter((dish) => dish.id === this.state.SeletedDish)[0]} />
+        <Footer />
+        </div>
+    );
+  }  
+}
+
+export default Main;
+   
