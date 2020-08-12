d1136259905f8980f438cc20820bc556c60a4b77
Harsh Verma <harsh1999verma.hv@gmail.com>
2020-08-12 20:09:00 +0530

    footer and header updated

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -16,7 +16,13 @@ class Contact extends Component {
             email: '',
             agree: false,
             contactType : 'Tel.',
-            message: ''
+            message: '',
+            touched: {
+                firstname: false,
+                lastname: false,
+                telnum: false,
+                email: false
+            }
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -38,6 +44,12 @@ class Contact extends Component {
         console.log("Current State is :" + JSON.stringify(this.state));
         alert("Current State is :" + JSON.stringify(this.state));
         event.preventDefault();
+    }
+
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: {...this.state.touched,}
+        })
     }
     
     render() {
@@ -61,11 +73,11 @@ class Contact extends Component {
                     <div className="col-12 col-sm-4 offset-sm-1">
                             <h5>Our Address</h5>
                             <address>
-                            121, Clear Water Bay Road<br />
-                            Clear Water Bay, Kowloon<br />
-                            HONG KONG<br />
-                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                            121, Opposite of Big Bazar<br />
+		                    A.V. Road, V.V. Nagar<br />
+		                    Anand<br />
+                            <i className="fa fa-phone"></i>: +917096739314<br />
+                            <i className="fa fa-fax"></i>: +919574217550<br />
                             <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
                             </address>
                     </div>
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -18,11 +18,11 @@ function Footer(props) {
                 <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
-		              121, Clear Water Bay Road<br />
-		              Clear Water Bay, Kowloon<br />
-		              HONG KONG<br />
-		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
+		              121, Opposite of Big Bazar<br />
+		              A.V. Road, V.V. Nagar<br />
+		              Anand<br />
+		              <i className="fa fa-phone fa-lg"></i>: +917096739314 <br />
+		              <i className="fa fa-fax fa-lg"></i>: +919574217550<br />
 		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
                          confusion@food.net</a>
                     </address>
@@ -40,7 +40,7 @@ function Footer(props) {
             </div>
             <div className="row justify-content-center">             
                 <div className="col-auto">
-                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                    <p></p>
                 </div>
             </div>
         </div>
